14af184e00e056390ecb2b05653d27acabfd4556
sjq19 <33825825+sjq19@users.noreply.github.com>
2018-11-29 08:52:58 +0800

    test

---
+++ src/main/java/com/how2java/tmall/util/Page.java
@@ -0,0 +1,85 @@
+package com.how2java.tmall.util;
+
+public class Page {
+
+    private int start;
+    private  int count;
+    private int total;
+    private String param;
+
+    private static final int defaultCount = 5;
+
+    public int getStart() {
+        return start;
+    }
+    public void setStart(int start) {
+        this.start = start;
+    }
+    public int getCount() {
+        return count;
+    }
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public Page(){
+        count = defaultCount;
+    }
+    public Page(int start,int count){
+        this();
+        this.start=start;
+        this.count = count;
+    }
+
+    public boolean isHasPreviouse(){
+        if(start==0)
+            return false;
+        return true;
+    }
+    public boolean isHasNext(){
+        if(start==getLast())
+            return false;
+        return true;
+    }
+
+    public int getTotalPage(){
+        int totalPage;
+        if(0 == total % count)
+            totalPage = total/count;
+        else
+            totalPage=total/count+1;
+        return  totalPage;
+    }
+
+    @Override
+    public String toString() {
+        return "Page [start=" + start + ", count=" + count + ", total=" + total + ", getStart()=" + getStart()
+                + ", getCount()=" + getCount() + ", isHasPreviouse()=" + isHasPreviouse() + ", isHasNext()="
+                + isHasNext() + ", getTotalPage()=" + getTotalPage() + ", getLast()=" + getLast() + "]";
+    }
+
+    private int getLast() {
+        int last;
+        // 假设总数是50，是能够被5整除的，那么最后一页的开始就是45
+        if (0 == total % count)
+            last = total - count;
+            // 假设总数是51，不能够被5整除的，那么最后一页的开始就是50
+        else
+            last = total - total % count;
+        last = last<0?0:last;
+        return last;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+    public void setTotal(int total) {
+        this.total = total;
+    }
+    public String getParam() {
+        return param;
+    }
+    public void setParam(String param) {
+        this.param = param;
+    }
+}
